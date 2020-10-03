95a4ab51e3e9fe449f7fccf2a002f8dbc3c1579b
powderflask <powderflask@gmail.com>
2020-10-03 14:10:30 -0700

    bug fixes

--- django_move_model/settings.py
+++ django_move_model/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'origin.apps.ModelMigrateOriginConfig',
-    'destination.apps.ModelMigrateDestinationConfig',
+    'origin.apps.MoveModelOriginConfig',
+    'destination.apps.MoveModelDestinationConfig',
 ]
 
 MIDDLEWARE = [
